bd038ff767c320b17a4d7426c9b75fa8ab744c54
Сергей Караульных <karaulnih@gmail.com>
2022-05-28 03:16:05 +0500

    Create files

---
+++ src/Dictionary.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Dictionary {
+
+    public <T, R extends Comparable<? super R>> List<R> map(List<T> dictionary, List<T> words, Function<T, R> func) {
+        List<R> result = new ArrayList<>();
+        result = words.stream()
+                .map(value -> value.toString())
+                .map(value -> value.split(" "))
+                .flatMap(Arrays::stream)
+                .map(value -> func.apply((T) value))
+                .distinct()
+                .filter(value -> !dictionary.contains(value))
+                .sorted(Comparator.<R>naturalOrder())
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    public String createDictionary(String text) {
+        return text.chars()
+                .filter(i -> Character.isLetterOrDigit(i))
+                .mapToObj(i -> String.valueOf((char) i))
+                .map(String::toLowerCase)
+                .collect(Collectors.joining());
+    }
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем экземпляр класса словаря
+        Dictionary obj = new Dictionary();
+
+        // Создаем список слов с уже известными нам словами аборигенов
+        List<String> dictionary = new ArrayList<>();
+        dictionary.add("мороз");
+        dictionary.add("остаюсь");
+        dictionary.add("холодно");
+
+        // Создаем список нескольких листков с текстами, записанными аборигенами
+        List<String> texts = Arrays.asList("Однажды в студёную зимнюю пору я из лесу вышел, был сильный мороз.",
+                "Однажды в мороз мы гуляли с собакой.",
+                "Когда я гуляю зимой, особенно в мороз, я тепло одеваюсь.",
+                "Когда на улице холодно, я мерзну.");
+
+        // Получаем список новых слов, отсутствующих в уже имеющемся словаре
+        List<String> preDictionary = new ArrayList<>();
+        preDictionary = obj.map(dictionary, texts, obj::createDictionary);
+        preDictionary.forEach(System.out::println);
+
+    }
+}
